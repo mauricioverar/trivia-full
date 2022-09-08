dce0b6b32260249fca8a2a2e4e0b417e0a71c1f6
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-09-08 17:58:34 -0400

    solo users

--- routes/routes.js
+++ routes/routes.js
@@ -15,16 +15,16 @@ router.get('/', (req, res) => {
 })
 
 // Vamos a crear un middleware para ver si el usuario está logueado o no
-function protected_route(req, res, next) {
-  if (!req.session.user) {
-    req.flash('errors', 'You must log in first')
-    return res.redirect('/login')
-  }
-  // si llegamos hasta acá, guardamos el usuario de la sesión en una variable de los templates
-  res.locals.user = req.session.user
-  // finalmente, seguimos el camino original
-  next()
-}
+// function protected_route(req, res, next) {
+//   if (!req.session.user) {
+//     req.flash('errors', 'You must log in first')
+//     return res.redirect('/login')
+//   }
+//   // si llegamos hasta acá, guardamos el usuario de la sesión en una variable de los templates
+//   res.locals.user = req.session.user
+//   // finalmente, seguimos el camino original
+//   next()
+// }
 
 
 // index GET
@@ -45,90 +45,90 @@ function protected_route(req, res, next) {
 
 
 // new_question GET
-router.get('/new_question', protected_route, (req, res) => {
-  res.render('new_question.html')
-})
+// router.get('/new_question', protected_route, (req, res) => {
+//   res.render('new_question.html')
+// })
 
 
 // new_question POST
-router.post('/new_question', protected_route, async (req, res) => {
-  try {
+// router.post('/new_question', protected_route, async (req, res) => {
+//   try {
 
-    if (req.session.user.is_admin == true) {
-      const question = req.body.question.trim()
-      const answer_true = req.body.answer_true.trim()
-      const false1 = req.body.answer_false1.trim()
-      const false2 = req.body.answer_false2.trim()
-      const false3 = req.body.answer_false3.trim()
-      const false4 = req.body.answer_false4.trim()
-      await create_question(question, answer_true, false1, false2, false3, false4)
-    }
-    res.redirect('/new_question')
+//     if (req.session.user.is_admin == true) {
+//       const question = req.body.question.trim()
+//       const answer_true = req.body.answer_true.trim()
+//       const false1 = req.body.answer_false1.trim()
+//       const false2 = req.body.answer_false2.trim()
+//       const false3 = req.body.answer_false3.trim()
+//       const false4 = req.body.answer_false4.trim()
+//       await create_question(question, answer_true, false1, false2, false3, false4)
+//     }
+//     res.redirect('/new_question')
 
-  } catch (error) {
-    console.log(error)
-  }
+//   } catch (error) {
+//     console.log(error)
+//   }
 
-})
+// })
 
 
 // lets_play GET
-router.get('/lets_play', protected_route, async (req, res) => {
-  try {
+// router.get('/lets_play', protected_route, async (req, res) => {
+//   try {
 
-    const dat = await get_questions()
-    await get_answer(dat)
-    res.render('lets_play.html', { dat })
+//     const dat = await get_questions()
+//     await get_answer(dat)
+//     res.render('lets_play.html', { dat })
 
-  } catch (error) {
-    console.log(error)
-  }
-})
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
 // answers del juego POST
-router.post('/lets_play', async (req, res) => {
+// router.post('/lets_play', async (req, res) => {
 
-  let result = 0
-  let percentage = 0
+//   let result = 0
+//   let percentage = 0
 
-  try {
-    const question1 = req.body.question1
-    const question2 = req.body.question2
-    const question3 = req.body.question3
+//   try {
+//     const question1 = req.body.question1
+//     const question2 = req.body.question2
+//     const question3 = req.body.question3
 
-    const user_id = req.session.user.id
-    if (question1 == '1') {
-      result++
-    }
-    if (question2 == '1') {
-      result++
-    }
-    if (question3 == '1') {
-      result++
-    }
+//     const user_id = req.session.user.id
+//     if (question1 == '1') {
+//       result++
+//     }
+//     if (question2 == '1') {
+//       result++
+//     }
+//     if (question3 == '1') {
+//       result++
+//     }
 
-    percentage = ((result * 100) / 3).toFixed(1)
-    await create_game(result, percentage, user_id)
-    req.session.user.play = true
-    res.redirect('/')
+//     percentage = ((result * 100) / 3).toFixed(1)
+//     await create_game(result, percentage, user_id)
+//     req.session.user.play = true
+//     res.redirect('/')
 
-  } catch (error) {
-    console.log(error)
-  }
-})
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
 // /search
-router.post('/search', (req, res) => {
-  try {
+// router.post('/search', (req, res) => {
+//   try {
 
-    req.session.name_us = req.body.nombre.trim()
-    res.redirect('/')
+//     req.session.name_us = req.body.nombre.trim()
+//     res.redirect('/')
 
-  } catch (error) {
-    console.log(error)
-  }
+//   } catch (error) {
+//     console.log(error)
+//   }
 
-})
+// })
 
 // 404 GET>
 router.get('*', (req, res) => {
