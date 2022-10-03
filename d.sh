d5169fcbb08f02cd56080c7e8f2e84f92849a4c3
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-10-03 19:58:56 -0400

    secr

--- d.sh
+++ d.sh
@@ -2,7 +2,7 @@
 # https://trivia-full.herokuapp.com/login
 
 git add .
-git commit -m 'sec'
+git commit -m 'secr'
 git push
 git push heroku main
 # Este es un comentario en bash  ./d.sh
--- secrets.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  db_name: 'trivia',
-  db_pass: 'Universo1',
-  db_user: 'postgres',
-  app_name: 'trivia',
-  db_host: 'localhost',
-}
